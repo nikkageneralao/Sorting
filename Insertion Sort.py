c47d9e3f168163fec29fbb685a7bded242f81402
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-23 02:26:50 +0800

    insertion sort method

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -7,5 +7,12 @@
 # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
 ## Insertion sort
+def insertion_sort(my_array):
+    for i in range(1, len(my_array)):
+        j = i
+        while my_array[j - 1] > my_array[j] and j > 0:
+            my_array[j - 1], my_array[j] = my_array[j], my_array[j-1]
+            j -= 1
+
 
 my_array = [30, 40, 98, 94, 77, 43, 24, 9, 93, 82]
