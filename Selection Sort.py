8c00fc111b5436c159db72d42b9d1473b5188d61
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-23 01:41:51 +0800

    selection sort method

--- Selection Sort.py
+++ Selection Sort.py
@@ -7,5 +7,15 @@
 # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
 ## Selection sort
+def sort(my_array):
+    for i in range (9):
+        min_position = i
+        for j in range(i, 10):
+            if my_array[j] < my_array[min_position]:
+                min_position = j
+
+        temp_position = my_array[i]
+        my_array[i] = my_array[min_position]
+        my_array[min_position] = temp_position
 
 my_array = [30, 40, 98, 94, 77, 43, 24, 9, 93, 82]
