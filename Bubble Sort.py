0619e661854351f10599d9fb15edd6e30a3ef166
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-23 02:02:24 +0800

    bubble sort method

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -7,5 +7,13 @@
 # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
 ## Bubble sort
+def sort(my_array):
+    for i in range(len(my_array) - 1, 0, -1):
+        for j in range(i):
+            if my_array[j] > my_array[j + 1]:
+                temp_position = my_array[j]
+                my_array[j] = my_array[j + 1]
+                my_array[j + 1] = temp_position
+
 
 my_array = [30, 40, 98, 94, 77, 43, 24, 9, 93, 82]
