f8104d645fe2a648212d95a9990ff0901f637977
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-23 22:43:38 +0800

    print

--- Merge Sort.py
+++ Merge Sort.py
@@ -37,4 +37,8 @@ def merge_sort(my_array):
             j += 1
             k += 1
 
+            print(my_array)
+
 my_array = [30, 40, 98, 94, 77, 43, 24, 9, 93, 82]
+merge_sort(my_array)
+print(my_array)
