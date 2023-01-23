ae184d611cb81c282917a50b8fcb25be850adecc
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-23 22:42:40 +0800

    merge sort method

--- Merge Sort.py
+++ Merge Sort.py
@@ -7,6 +7,34 @@
 # _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
 ## Merge sort
+def merge_sort(my_array):
+    if len(my_array) > 1:
+        left_array = my_array[:len(my_array) // 2]
+        right_array = my_array[len(my_array) // 2:]
 
+        merge_sort(left_array)
+        merge_sort(right_array)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                my_array[k] = left_array[i]
+                i += 1
+            else:
+                my_array[k] = right_array[j]
+                j += 1
+            k += 1
+
+        while i < len(left_array):
+            my_array[k] = left_array[i]
+            i += 1
+            k += 1
+
+        while j < len(right_array):
+            my_array[k] = right_array[j]
+            j += 1
+            k += 1
 
 my_array = [30, 40, 98, 94, 77, 43, 24, 9, 93, 82]
