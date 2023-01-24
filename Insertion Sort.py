58aebf523b1f2a92c65426e8ca47737871c28b1d
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-24 23:13:53 +0800

    Finish

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -8,15 +8,20 @@
 
 ## Insertion sort
 def insertion_sort(my_array):
+    print("\n\t\t ------- Insertion Sort Algorithm ------")
     for i in range(1, len(my_array)):
         j = i
         while my_array[j - 1] > my_array[j] and j > 0:
             my_array[j - 1], my_array[j] = my_array[j], my_array[j-1]
             j -= 1
 
-            print(my_array)
+            print("\t\t", my_array)
 
 
 my_array = [30, 40, 98, 94, 77, 43, 24, 9, 93, 82]
+print("\n---------------------- INSERTION SORT ---------------------")
+print("\nUnsorted List:", my_array)
 insertion_sort(my_array)
-print(my_array)
+print("\t\t ---------------------------------------")
+print("\nSorted List:", my_array)
+print("\n--------------------------------------------------------")
