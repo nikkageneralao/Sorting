8c2e7d6aaf9db236c7e66ff46db71ea6db96b353
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-01-26 01:29:21 +0800

    Finish

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -9,6 +9,7 @@
 ## Bubble sort
 def bubble_sort(my_array):
     print("\n\t\t --------- Bubble Sort Algorithm -------")
+    print("\t\t", my_array)
     for i in range(len(my_array) - 1, 0, -1):
         for j in range(i):
             if my_array[j] > my_array[j + 1]:
